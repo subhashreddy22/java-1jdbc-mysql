76e487964b5daf0ea3a461397200a42552fb7455
subhashreddy22 <subhashreddy22@gmail.com>
2018-09-17 23:35:23 +0530

    Simple JDBC

---
+++ src/com/jdbc/JdbcConnection.java
@@ -0,0 +1,27 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcConnection {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/subhash", "root", "root");
+
+		Statement statement = con.createStatement();
+
+		ResultSet resultSet = statement.executeQuery("select * from student");
+
+		while (resultSet.next())
+			System.out.println(resultSet.getInt(1) + "  " + resultSet.getString(2));
+		
+		con.close();
+
+	}
+}
